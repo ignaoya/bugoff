272b3476306cba81e92a2a8f5f9dc10317281f99
Ignacio Oyarzabal <ignaoya93@gmail.com>
2020-05-23 15:33:52 +0200

    added pagination to list of projects page

--- tracker/views.py
+++ tracker/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Project, Bug, Worknote
 
 
@@ -7,8 +8,18 @@ def index(request):
     return HttpResponse("Welcome to Bugoff! The online Python bug tracker.")
 
 def project_list(request):
-    projects = Project.objects.all()
-    return render(request, 'tracker/project/list.html', {'projects': projects})
+    object_list = Project.objects.all()
+    paginator = Paginator(object_list, 1) # 3 projects in each page
+    page = request.GET.get('page')
+    try:
+        projects = paginator.page(page)
+    except PageNotAnInteger:
+        # if page is not an integer deliver the first page
+        projects = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        projects = paginator.page(paginator.num_pages)
+    return render(request, 'tracker/project/list.html', {'page': page, 'projects': projects})
 
 def project_detail(request, name):
     project = get_object_or_404(Project, name=name)
